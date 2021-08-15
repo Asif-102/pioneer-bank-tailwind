faf466dea6c0a32f29d4a459102e78dfa6a008ee
Asif2535 <aiqbal517@gmail.com>
2021-08-16 02:42:33 +0600

    initial part completed

--- js/banking.js
+++ js/banking.js
@@ -1,8 +1,8 @@
 //handle deposit button event
-document.getElementById('deposit-button').addEventListener('click', function(){
+document.getElementById('deposit-button').addEventListener('click', function () {
     //get the amount deposited
     const depositAmount = document.getElementById('deposit-input');
-    
+
     const depositTotal = document.getElementById('deposit-total');
     depositTotal.innerText = parseFloat(depositTotal.innerText) + parseFloat(depositAmount.value);
 
@@ -12,4 +12,23 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
     //clear the deposit input field;
     depositAmount.value = '';
+});
+
+// handle withdraw event handler
+document.getElementById('withdraw-button').addEventListener('click', function () {
+
+    const withdrawAmount = document.getElementById('withdraw-input');
+
+    //set withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawAmount.value);
+
+    //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    balanceTotal.innerText = parseFloat(balanceTotal.innerText) - parseFloat(withdrawAmount.value);
+
+    //clear the withdraw input field;
+    withdrawAmount.value = '';
+
+
 })
